03f431a2474054c4775f5b5417e4d5ab787975ac
railken <railken@protonmail.ch>
2019-05-04 17:32:34 +0200

    update

--- src/Providers/CollectionServiceProvider.php
+++ src/Providers/CollectionServiceProvider.php
@@ -13,6 +13,14 @@ class CollectionServiceProvider extends CommonServiceProvider
     public function register()
     {
         parent::register();
+    }
+
+    /**
+     * @inherit
+     */
+    public function boot()
+    {
+        parent::boot();
 
         \Illuminate\Database\Eloquent\Builder::macro('collectionItems', function (): MorphMany {
             return app('amethyst')->createMacroMorphRelation($this, \Railken\Amethyst\Models\CollectionItem::class, 'collectionItems', 'collectionable');
