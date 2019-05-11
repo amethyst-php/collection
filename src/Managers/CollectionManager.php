52ae96446fdc14c4d6539379be3ecd23df050b55
railken <railken@protonmail.ch>
2019-05-11 17:01:10 +0200

    fix style

--- src/Managers/CollectionManager.php
+++ src/Managers/CollectionManager.php
@@ -23,7 +23,7 @@ class CollectionManager extends Manager
     {
         return [
             'tree' => [
-                'parent'   => 'parent_id',
+                'parent' => 'parent_id',
             ],
         ];
     }
